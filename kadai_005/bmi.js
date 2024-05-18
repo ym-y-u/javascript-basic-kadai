e62b2e3d50a3e549450006890912891f1a809654
ym-y-u <166213577+ym-y-u@users.noreply.github.com>
2024-05-18 09:46:38 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,9 @@
 //変数の宣言・値の代入
-let bmi = [68]/[1.7*1.7]
+let weight = 68
+let height = 1.7
+let bmi = weight/(height*height)
 
 //コンソールへの出力
+console.log(weight);
+console.log(height);
 console.log(bmi);
