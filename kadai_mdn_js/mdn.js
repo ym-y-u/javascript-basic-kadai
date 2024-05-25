991aa4fe96e7cea027d57e0e9faed7ac0b86e6f3
ym-y-u <166213577+ym-y-u@users.noreply.github.com>
2024-05-25 10:17:32 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const date = new Date();
+const [month, day, year] = [
+  date.getMonth(),
+  date.getDate(),
+  date.getFullYear(),
+];
+
+const formattedDate = `${year}年${month}月${day}日`;
+
+console.log(formattedDate);
